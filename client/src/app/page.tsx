4536552d04d115bb75f995097c22e855fa31ed1f
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-24 14:28:39 +0800

    chore: applied the theme color in the home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,9 +7,14 @@ export default function Home() {
       <Head>
         <title>Tic Tac Toe - Home</title>
       </Head>
-      <main className="flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-4xl font-bold mb-4">Tic Tacc Toe</h1>
-        <Link href="/new-game" className="px-4 py-2 bg blue-500 text-white rounded hover:bg-blue-600">
+      <main className="min-h-screen flex flex-col items-center justify-center bg-[var(--background)] text-[var(--foreground)] p-4">
+        <h1 className="text-4xl font-bold mb-6 text-[var(--primary)]">
+          Tic-Tac-Toe Game
+        </h1>
+        <Link
+          href="/new-game"
+          className="px-6 py-3 bg-[var(--secondary)] hover:bg-[var(--hover)] text-white rounded-xl"
+        >
           Start New Game
         </Link>
       </main>
