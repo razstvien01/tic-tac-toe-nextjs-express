d8231e581eb2d4b7ee380b06fab37561bd08eab8
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-24 14:28:14 +0800

    chore: applied the theme color in new game page

--- client/src/app/new-game/page.tsx
+++ client/src/app/new-game/page.tsx
@@ -3,42 +3,47 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 
 export default function NewGame() {
-  const router = useRouter()
-  const [player1, setPlayer1] = useState('');
-  const [player2, setPlayer2] = useState('');
-  
+  const router = useRouter();
+  const [player1, setPlayer1] = useState("");
+  const [player2, setPlayer2] = useState("");
+
   const startGame = () => {
-    if(player1 && player2){
+    if (player1 && player2) {
       router.push({
         pathname: "/gameplay",
-        query: { player1, player2 }
-      })
+        query: { player1, player2 },
+      });
     }
-  }
-  
+  };
+
   return (
     <>
       <Head>
         <title>Start New Game</title>
       </Head>
-      <main className="flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-2xl font-bold mb-4">Enter Player Names</h1>
+      <main className="min-h-screen flex flex-col items-center justify-center bg-[var(--background)] text-[var(--foreground)] p-4">
+        <h1 className="text-3xl mb-4 text-[var(--primary)]">
+          Enter Player Names
+        </h1>
         <input
-          className="border p-2 mb-2 w-64"
+          className="mb-2 p-2 rounded-xl bg-[var(--hover)] text-white"
           placeholder="Player 1"
           value={player1}
-          onChange={e => setPlayer1(e.target.value)}
+          onChange={(e) => setPlayer1(e.target.value)}
         />
         <input
-          className="border p-2 mb-2 w-64"
+          className="mb-4 p-2 rounded-xl bg-[var(--hover)] text-white"
           placeholder="Player 2"
-          value={player1}
-          onChange={e => setPlayer2(e.target.value)}
+          value={player2}
+          onChange={(e) => setPlayer2(e.target.value)}
         />
-        <button onClick={startGame} className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+        <button
+          className="px-6 py-2 bg-[var(--secondary)] hover:bg-[var(--hover)] rounded-xl"
+          onClick={startGame}
+        >
           Start
         </button>
       </main>
     </>
-  )
+  );
 }
