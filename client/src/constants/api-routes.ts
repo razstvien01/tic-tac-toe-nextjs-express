63bcc9eeec499c8bbc08e80a81581eb168778b90
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-25 22:50:51 +0800

    chore: made API_ROUTES export as default

--- client/src/constants/api-routes.ts
+++ client/src/constants/api-routes.ts
@@ -1,6 +1,8 @@
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
-export const API_ROUTES = {
+const API_ROUTES = {
   SAVE_SESSION: `${BASE_URL}/api/save-session/`,
   GET_SESSIONS: `${BASE_URL}/api/get-sessions/`,
 };
+
+export default API_ROUTES;
