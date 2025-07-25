eaf98bba19f440a95a805a3785bba6383bae9284
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-25 19:09:43 +0800

    chore: implemented mapper

---
+++ client/src/mappers/game-session.mapper.ts
@@ -0,0 +1,14 @@
+import { GameSessionDto } from "@/dtos/game-session.dto";
+import { GameSession } from "@/models/game-session.model";
+
+export function mapGameSessionDtoToModel(dto: GameSessionDto): GameSession {
+  return {
+    ...dto,
+    startTime: new Date(dto.startTime),
+    endTime: new Date(dto.endTime),
+  };
+}
+
+export function mapGameSessionsDtoToModels(dtos: GameSessionDto[]) {
+  return dtos.map(mapGameSessionDtoToModel);
+}
