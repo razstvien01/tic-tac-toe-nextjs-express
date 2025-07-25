64859eabc1251f2f672fb81d1a5365c5d3f1f442
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-25 23:48:27 +0800

    feat: implemented connectDB func to connect mongodb

---
+++ server/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv").config({ path: ".env.local" });
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
