a99b668425f7a9118306c0a801efbf921920b154
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-25 22:51:33 +0800

    implemented save session api route func

--- server/index.js
+++ server/index.js
@@ -42,5 +42,11 @@ app.get("/api/game-sessions", (req, res) => {
   res.status(200).json(gameSessionDtos);
 });
 
+app.post("/api/save-session", (req, res) => {
+  console.log("Received session data:", req.body);
+  
+  res.status(200).json({ message: "Session saved" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
