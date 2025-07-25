0847bf6ac1dabd3e7431b9c9c8d1585252a5428d
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-25 22:15:17 +0800

    feat: implemented getGameSession service func

--- client/src/services/sessionService.ts
+++ client/src/services/sessionService.ts
@@ -1,15 +1,26 @@
+import { API_ROUTES } from "@/constants/api-routes";
 import { InGameSession } from "@/models/in-game-session.model";
 import axios from "axios";
 
 export const saveGameSession = async (session: InGameSession) => {
   try {
-    await axios.post("/api/save-session", session.toDto(), {
+    await axios.post(API_ROUTES.SAVE_SESSION, session.toDto(), {
       headers: {
         "Content-Type": "application/json",
       },
     });
   } catch (error) {
     console.error("Error saving session:", error);
+    throw error;
+  }
+};
+
+export const getGameSession = async () => {
+  try {
+    const result = await axios.get(API_ROUTES.GET_SESSIONS);
+    return result.data;
+  } catch (error) {
+    console.error("Error getting sessions:", error);
     throw error;
   }
 };
