eae2a19545727535c1c91ceea443e6fd87f04fb2
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-07-27 02:13:48 +0800

    chore: removed based url and trailing slash

--- client/src/constants/api-routes.ts
+++ client/src/constants/api-routes.ts
@@ -1,8 +1,7 @@
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 const API_ROUTES = {
-  SAVE_SESSION: `${BASE_URL}/api/save-session/`,
-  GET_SESSIONS: `${BASE_URL}/api/get-sessions/`,
+  SAVE_SESSION: `/api/save-session`,
+  GET_SESSIONS: `/api/get-sessions`,
 };
 
 export default API_ROUTES;
